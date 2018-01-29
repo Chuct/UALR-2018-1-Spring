2ba1bbcb8588732acc113b7c5befb8ab352d8e1d
chuct <chuctlr@gmail.com>
2018-01-29 16:44:44 -0600

    Update 1_approximating_pi.cpp

--- Special Topics - Monte Carlo Simulation/hw/1_approximating_pi.cpp
+++ Special Topics - Monte Carlo Simulation/hw/1_approximating_pi.cpp
@@ -3,17 +3,34 @@
 #include <string>
 using namespace std;
 
-const int seeds = 1000;
+const int seeds = 100; // Only for simulateAverage()
 const int trials = 100000;
 
 void simulate();
+void simulateAverage();
 
 void main()
 {
 	simulate();
+	simulateAverage(); // Just playing around a bit...
 }
 
 void simulate()
+{
+	int inCircle;
+	double randX, randY, pi = 0;
+	for (int i = 0; i < trials; i++)
+	{
+		randX = (double)rand() / RAND_MAX;
+		randY = (double)rand() / RAND_MAX;
+		if (((randX * randX) + (randY * randY)) <= 1)
+			inCircle++;
+	}
+	pi = pi + ((double)(4 * inCircle) / trials);
+	cout << "Pi has been approximated to: " << pi << endl;
+}
+
+void simulateAverage()
 {
 	int inCircle;
 	double randX, randY, pi = 0;
@@ -31,6 +48,6 @@ void simulate()
 		inCircle = 0;
 	}
 	pi = pi / seeds;
-	cout << "Pi has been approximated to: " << pi << endl;
+	cout << "Pi has been approximated AND AVERAGED to: " << pi << " using seeds 1-" << seeds << endl;
 	system("pause");
 }
